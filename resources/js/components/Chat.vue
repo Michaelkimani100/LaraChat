dabe3eb4b09d922a019e51153346ce91b40f2e3a
Michaelkimani100 <michaelkimani854@gmail.com>
2019-09-29 16:50:56 +0300

    Front-end-setup

---
+++ resources/js/components/Chat.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center mt-3">
+            <div class="col-md-4">
+            <ul class="list-group">
+            <li class="list-group-item active">Chat App</li>
+            <li class="list-group-item">Dapibus ac facilisis in</li>
+            <li class="list-group-item">Morbi leo risus</li>
+            <li class="list-group-item">Porta ac consectetur ac</li>
+            <li class="list-group-item">Vestibulum at eros</li>
+            <input type="text" class="form-group" placeholder="Type here...">
+            </ul>
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
