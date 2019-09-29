69f246cdcbcd227e840270cc680ffa9bac717f6e
Michaelkimani100 <michaelkimani854@gmail.com>
2019-09-29 22:21:14 +0300

    vue-chat-scroll

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,17 +7,12 @@
 require('./bootstrap');
 
 window.Vue = require('vue');
+import Vue from 'vue'
 
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
+import VueChatScroll from 'vue-chat-scroll'
+Vue.use(VueChatScroll)
 
-// const files = require.context('./', true, /\.vue$/i);
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
+
 
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 Vue.component('chat', require('./components/Chat.vue').default);
--- resources/js/components/Chat.vue
+++ resources/js/components/Chat.vue
@@ -1,16 +1,19 @@
 <template>
     <div class="container">
         <div class="row justify-content-center mt-3">
-            <div class="col-md-4">
-            <ul class="list-group">
-            <li class="list-group-item active">Chat App</li>
-            <li class="list-group-item">Dapibus ac facilisis in</li>
-            <li class="list-group-item">Morbi leo risus</li>
-            <li class="list-group-item">Porta ac consectetur ac</li>
-            <li class="list-group-item">Vestibulum at eros</li>
-            <input type="text" class="form-group" placeholder="Type here...">
-            </ul>
+            <div class="col-md-6">
+             <li class="list-group-item active text-center">Chat App</li>
+            <ul class="list-group" v-chat-scroll >
+            <div v-for="value in chat.message" :key="value.index">
+             <li class="list-group-item list-group-item-success text-success" >{{ value }}
+             </li>
+             <small class="badge float-right badge-success">You</small>
+            </div>
 
+            </ul>
+            <div class="form-group">
+              <input type="text" class="form-control" placeholder="Type here..." name="message" v-model="message" @keyup.enter="save">
+            </div>
             </div>
         </div>
     </div>
@@ -18,8 +21,37 @@
 
 <script>
     export default {
+        data(){
+            return{
+                message:'',
+                chat:{
+                    message:[]
+                },
+
+            }
+
+        },
+
         mounted() {
             console.log('Component mounted.')
+        },
+        methods:{
+            save(){
+                if(this.message.length!=0)
+                {
+                    this.chat.message.push(this.message)
+                    this.message=''
+
+                }
+
+            }
         }
+
     }
 </script>
+<style scoped>
+.list-group{
+    overflow-y: scroll;
+    height: 200px;
+}
+</style>
